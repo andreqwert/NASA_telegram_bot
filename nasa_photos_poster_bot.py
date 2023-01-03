f7ea66741cff8d1e8945c979be4d76b87e72e4fd
Andrey <andrew.aslanov@gmail.com>
2023-01-04 01:48:11 +0300

    Добавил описание к функциям, поменял spaces->tabs

--- nasa_photos_poster_bot.py
+++ nasa_photos_poster_bot.py
@@ -17,48 +17,54 @@ def parse_args():
 
 
 def get_random_image_path(dir_with_images):
+    """
+    Получаем список файлов из папки dir_with_images и перемешиваем его.
+    На выход подаем первый файл из списка"""
 
-	image_paths = []
-	for root, dirs, files in os.walk(os.path.abspath(dir_with_images)):
-	    for file in files:
-	    	if not file.startswith('.'):
-	        	image_paths.append(os.path.join(root, file))
-	random.shuffle(image_paths)
-	return image_paths[0]
+    image_paths = []
+    for root, dirs, files in os.walk(os.path.abspath(dir_with_images)):
+        for file in files:
+            if not file.startswith('.'):
+                image_paths.append(os.path.join(root, file))
+    random.shuffle(image_paths)
+    return image_paths[0]
 
 
 def check_file_under_limit(file_path, limit=20.0):
+    """
+    Проверяем, действительно ли файл по указанному пути.
+    Если да, то занимает ли он больше limit мб."""
 
-	isfile = os.path.isfile(file_path)
-	if isfile:
-		file_size_mb = os.stat(file_path).st_size / 1e6
-		return file_size_mb < float(limit)
-	else:
-		return False
+    isfile = os.path.isfile(file_path)
+    if isfile:
+        file_size_mb = os.stat(file_path).st_size / 1e6
+        return file_size_mb < float(limit)
+    else:
+        return False
 
 
 def main():
 
-	args = parse_args()
-	images_dir = args.images_dir
-	limit_mb = args.limit_mb
+    args = parse_args()
+    images_dir = args.images_dir
+    limit_mb = args.limit_mb
 
-	env = Env()
-	env.read_env()
-	telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
-	telegram_chat_id = env('TELEGRAM_CHAT_ID')
-	publication_freq_seconds = int(env('PUBLICATION_FREQ_SECONDS'))
-	bot = telegram.Bot(token=telegram_bot_token)
+    env = Env()
+    env.read_env()
+    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
+    telegram_chat_id = env('TELEGRAM_CHAT_ID')
+    publication_freq_seconds = int(env('PUBLICATION_FREQ_SECONDS'))
+    bot = telegram.Bot(token=telegram_bot_token)
 
-	while True:
-		random_image_path = get_random_image_path(images_dir)
-		file_under_limit = check_file_under_limit(random_image_path, limit_mb)
-		if file_under_limit:
-			image_to_send = InputMediaPhoto(media=open(random_image_path, 'rb'))
-			time.sleep(publication_freq_seconds)
-			bot.send_media_group(chat_id=-1001643643093, media=[image_to_send])
-	
+    while True:
+        random_image_path = get_random_image_path(images_dir)
+        file_under_limit = check_file_under_limit(random_image_path, limit_mb)
+        if file_under_limit:
+            image_to_send = InputMediaPhoto(media=open(random_image_path, 'rb'))
+            time.sleep(publication_freq_seconds)
+            bot.send_media_group(chat_id=-1001643643093, media=[image_to_send])
+    
 
 if __name__ == '__main__':
-	main()
+    main()
 
