3160bef17c1a93ec642723d8d86dd48c579224a5
Andrey <andrew.aslanov@gmail.com>
2023-01-16 16:14:22 +0300

    Скрыл ID чата в .env, добавил библиотеку для обработки путей

--- nasa_one_image_poster_bot.py
+++ nasa_one_image_poster_bot.py
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import telegram
-import requests
-from telegram import InputMediaPhoto
-from environs import Env
-import argparse
-import time
-from data_processing import convert_to_mb, get_random_image_path, check_file_under_limit
-
-
-def parse_args():
-
-    parser = argparse.ArgumentParser(description='Бот для постинга фото. Если явно фото не укзаано, то публикается случайное фото.')
-    parser.add_argument('--user_image_path', help='Путь до изображения для публикации. Если флаг указан, то это из-е будет загружено первым.')
-    parser.add_argument('--limit_mb', default=20.0, type=float, help='Лимит (в мб.) для изображения, подлежащего публикации')
-    parser.add_argument('--retry_delay_seconds', default=5, type=int, help='Через сколько секунд будет произведен повторный запрос при потере соединения')
-    args = parser.parse_args()
-    return args
-
-
-def main():
-
-    env = Env()
-    env.read_env()
-    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
-    telegram_chat_id = env('TELEGRAM_CHAT_ID')
-    bot = telegram.Bot(token=telegram_bot_token)
-
-    args = parse_args()
-    user_image_path = args.user_image_path
-    limit_mb = args.limit_mb
-    retry_delay_seconds = args.retry_delay_seconds
-
-    try:
-        if user_image_path:
-            image_path = user_image_path
-        else:
-            image_path = get_random_image_path(images_dir)
-        file_under_limit = check_file_under_limit(image_path, limit_mb)
-        if file_under_limit:
-            with open(image_path, 'rb') as img:
-                image_to_send = InputMediaPhoto(media=img)
-            bot.send_media_group(chat_id=telegram_chat_id, media=[image_to_send])
-    except telegram.error.NetworkError:
-        print(f"Received ConnectionError. Retrying in {retry_delay_seconds} seconds")
-        telegram.error.RetryAfter(retry_delay_seconds)
-    
-
-if __name__ == '__main__':
-    main()
-
-=======
 import telegram
 import requests
 from telegram import InputMediaPhoto
@@ -58,13 +6,14 @@ import argparse
 import time
 from data_processing import convert_to_mb, get_random_image_path, check_file_under_limit
 import os
+from pathlib import Path
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser(description='Бот для постинга фото. Если явно фото не укзаано, то публикается случайное фото.')
     parser.add_argument('--user_image_path', help='Путь до изображения для публикации. Если флаг указан, то это из-е будет загружено первым.')
-    parser.add_argument('--images_dir', default='./images/', help='Путь до папки с картинками, подлежащими публикации (в случае, если не указано определенное фото).')
+    parser.add_argument('--images_dir', default='./images/', type=Path, help='Путь до папки с картинками, подлежащими публикации (в случае, если не указано определенное фото).')
     parser.add_argument('--limit_mb', default=20.0, type=float, help='Лимит (в мб.) для изображения, подлежащего публикации')
     parser.add_argument('--retry_delay_seconds', default=5, type=int, help='Через сколько секунд будет произведен повторный запрос при потере соединения')
     args = parser.parse_args()
@@ -95,7 +44,7 @@ def main():
         if file_under_limit:
             with open(image_path, 'rb') as img:
                 image_to_send = InputMediaPhoto(media=img)
-            bot.send_media_group(chat_id=178680093, media=[image_to_send])
+            bot.send_media_group(chat_id=telegram_chat_id, media=[image_to_send])
     except telegram.error.NetworkError:
         print(f"Received ConnectionError. Retrying in {retry_delay_seconds} seconds")
         telegram.error.RetryAfter(retry_delay_seconds)
--- nasa_photos_poster_bot.py
+++ nasa_photos_poster_bot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import telegram
 import requests
 from telegram import InputMediaPhoto
@@ -7,12 +6,13 @@ import os
 import argparse
 import time
 from data_processing import convert_to_mb, get_random_image_path, check_file_under_limit
+from pathlib import Path
 
 
 def parse_args():
 
     parser = argparse.ArgumentParser(description='Бот для постинга фото. Публикаются случайные фото из папки.')
-    parser.add_argument('--images_dir', default='./images/', help='Путь до папки с картинками, подлежащими публикации (в случае, если не указано определенное фото).')
+    parser.add_argument('--images_dir', default='./images/', type=Path, help='Путь до папки с картинками, подлежащими публикации (в случае, если не указано определенное фото).')
     parser.add_argument('--limit_mb', default=20.0, type=float, help='Лимит (в мб.) для изображения, подлежащего публикации')
     parser.add_argument('--publication_freq_sec', default=5, type=int, help='Частота публикации в канале Telegram (в секундах)')
     parser.add_argument('--retry_delay_seconds', default=5, type=int, help='Через сколько секунд будет произведен повторный запрос при потере соединения')
@@ -52,60 +52,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-=======
-import telegram
-import requests
-from telegram import InputMediaPhoto
-from environs import Env
-import os
-import argparse
-import time
-from data_processing import convert_to_mb, get_random_image_path, check_file_under_limit
-
-
-def parse_args():
-
-    parser = argparse.ArgumentParser(description='Бот для постинга фото. Публикаются случайные фото из папки.')
-    parser.add_argument('--images_dir', default='./images/', help='Путь до папки с картинками, подлежащими публикации (в случае, если не указано определенное фото).')
-    parser.add_argument('--limit_mb', default=20.0, type=float, help='Лимит (в мб.) для изображения, подлежащего публикации')
-    parser.add_argument('--publication_freq_sec', default=5, type=int, help='Частота публикации в канале Telegram (в секундах)')
-    parser.add_argument('--retry_delay_seconds', default=5, type=int, help='Через сколько секунд будет произведен повторный запрос при потере соединения')
-    args = parser.parse_args()
-    return args
-
-
-def main():
-
-    env = Env()
-    env.read_env()
-    telegram_bot_token = env('TELEGRAM_BOT_TOKEN')
-    telegram_chat_id = env('TELEGRAM_CHAT_ID')
-    bot = telegram.Bot(token=telegram_bot_token)
-
-    args = parse_args()
-    images_dir = args.images_dir
-    limit_mb = args.limit_mb
-    publication_freq_seconds = args.publication_freq_sec
-    retry_delay_seconds = args.retry_delay_seconds
-
-    assert len(os.listdir(images_dir)) > 0, 'Images directory is empty'
-
-    while True:
-        try:
-            image_path = get_random_image_path(images_dir)
-            file_under_limit = check_file_under_limit(image_path, limit_mb)
-            if file_under_limit:
-                with open(image_path, 'rb') as img:
-                    image_to_send = InputMediaPhoto(media=img)
-                time.sleep(publication_freq_seconds)
-                bot.send_media_group(chat_id=178680093, media=[image_to_send])
-        except telegram.error.NetworkError:
-            print(f"Received ConnectionError. Retrying in {retry_delay_seconds} seconds...")
-            telegram.error.RetryAfter(retry_delay_seconds)
-    
-
-if __name__ == '__main__':
-    main()
-
->>>>>>> bf87858 (Добавил аргументы в README, разделил функционал по скриптам)
